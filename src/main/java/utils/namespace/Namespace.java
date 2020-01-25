89a62ee17dc3301da458059366cee10094a541db
kruk1 <kruk162@gmail.com>
2020-01-25 14:18:26 +0100

    task4

--- src/main/java/utils/namespace/Namespace.java
+++ src/main/java/utils/namespace/Namespace.java
@@ -2,6 +2,9 @@ package controllers.servlets.namespace;
 
 public class Namespace {
 
+        public static final String ERROR_ATTRIBUTE_NAME = "errors";
+
+
         public static final String USER_LOGIN = "login";
         public static final String USER_NAME = "name";
         public static final String USER_SURNAME = "lastName";
@@ -9,16 +12,28 @@ public class Namespace {
         public static final String USER_PASSWORD = "password";
         public static final String USER_REPEATED_PASSWORD = "repeatedPassword";
 
+        public static final String REMEBER = "remeber";
+        public static final String LOGIN_COOKIE = "remeber";
+        public static final String PASSWORD_COOKIE = "remeber";
+        public static final String CHECKBOOK_CHECKED = "ON";
+
+
+
+
         public static final String PASSWORD_ERROR_HEADER = "INVALID PASSWORD";
-        public static final String PASSWORD_ERROR_MESSAGE = "Passwords not be equals";
         public static final String LOGIN_ERROR_HEADER = "INVALID LOGIN";
-        public static final String LOGIN_ERROR_MESSAGE = "Typed login is already in use";
         public static final String EMAIL_ERROR_HEADER = "INVALID EMAIL";
+
+        public static final String LOGIN_NOT_EXIST_ERROR_MESSAGE = "Typed login do not exist";
+        public static final String LOGIN_IN_USE_ERROR_MESSAGE = "Typed mail is already in use";
+
+        public static final String WORNG_PASSWORD_ERROR_MESSAGE ="Typed login is aleardy in use";
         public static final String EMAIL_ERROR_MESSAGE = "Typed mail is already in use";
 
+        public static final String PASSWORD_ERROR_MESSAGE = "Passwords not be equals";
 
-        public static final String LOGIN_OR_PASSWORD_ERROR_HEADER = "Error";
-        public static final String LOGIN_OR_PASSWORD_ERROR_MESSAGE = "Invalid login or password";
+
+
 
         public static final String LOGGED_USER = "loggedUser";
 }
